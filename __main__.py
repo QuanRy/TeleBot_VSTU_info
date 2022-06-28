8b2213855fd0e9f78315d1177030483ea76a76d2
QuanRy <kacman1964@mail.ru>
2022-06-28 17:01:13 +0300

    Refactoring code

--- __main__.py
+++ __main__.py
@@ -1,19 +1,33 @@
 import telebot
+import random
+import datetime
 import numpy as np
 
-bot = telebot.TeleBot("5525229543:AAF5zhi0s34PWgg0x3ufwdEAnxrrgCCLpjY", parse_mode=None)
+TOKEN = "5525229543:AAF5zhi0s34PWgg0x3ufwdEAnxrrgCCLpjY"  # токен для подключения к Боту
 
-@bot.message_handler(func=lambda message: True) # функция "попугай"
-def echo_all(message):
-	bot.reply_to(message, message.text)
-
-@bot.message_handler(commands=['start', 'help'])
-def send_welcome(message):
-	bot.reply_to(message, "Я тебе НЕ бот!")
+bot = telebot.TeleBot(TOKEN, parse_mode=None)
 
 @bot.message_handler(content_types=['text'])   # функция для удаления сообщения  сзапретным словом
 def chatting(message):
+
+    if message.text == '/start' or  message.text == '/help': 
+        bot.reply_to(message, 'Я тебе НЕ бот!')
+
     if message.text.lower() =='слово':
         bot.delete_message(message.chat.id, message.message_id)
+
+    if  message.text == '/random':
+        bot.send_message(message.from_user.id, random.randint(0,9))
+    
+    if  message.text == '/time':
+        bot.send_message(message.from_user.id, str(datetime.datetime.now()))
+
+
+
+
+
+# @bot.message_handler(func=lambda message: True) # функция "попугай"
+# def echo_all(message):
+# 	bot.reply_to(message, message.text)
 
 bot.infinity_polling()
